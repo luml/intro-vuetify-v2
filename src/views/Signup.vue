1b4183ac1f74ed953b6b70b5db746db3c5144690
luml <1520622848@qq.com>
2021-09-13 12:03:52 +0800

    feat: validate

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -3,14 +3,17 @@
     <v-row class="mx-auto mt-5">
       <v-col>
         <h1>Sign up</h1>
-        <v-form>
-          <v-text-field label="Email" type="email"></v-text-field>
+        <v-form ref="signUpFrom" v-model="formValidity">
+          <v-text-field v-model="email" :rules="emailRules" label="Email" type="email" required></v-text-field>
           <v-autocomplete label="Which browser do you use?" :items="browsers"></v-autocomplete>
           <v-file-input label="Attach profile"></v-file-input>
           <v-date-picker v-model="birthday"></v-date-picker>
           <v-text-field label="Birthday" v-model="birthday" readonly></v-text-field>
-          <v-checkbox label="Agree to terms & conditions"></v-checkbox>
-          <v-btn type="submit" color="primary">submit</v-btn>
+          <v-checkbox v-model="agreeToTerms" :rules="agreeToTermsRules" label="Agree to terms & conditions"></v-checkbox>
+          <v-btn type="submit" color="primary" class="mr-4" :disabled="!formValidity">submit</v-btn>
+          <v-btn @click="validateForm" class="mr-4" color="warning">Validate Form</v-btn>
+          <v-btn color="warning" @click="resetValidation" class="mr-4">Reset Validation</v-btn>
+          <v-btn color="error" @click="resetForm">Reset</v-btn>
         </v-form>
       </v-col>
     </v-row>
@@ -20,8 +23,32 @@
 <script>
 export default {
   data: () => ({
-    browsers: ["Chrome", "Firefox", "Safari", "Brave"],
-    birthday: ""
-  })
+    email: '',
+    emailRules: [
+      value => !!value || 'Email is required',
+      value => value.indexOf('@') !== 0 || 'Email should have a username',
+      value => value.includes('@') || 'Email should include @ symbol',
+      value => value.indexOf('.') - value.indexOf('@') > 1 || 'Email should have a valid domain.',
+      value => value.indexOf('.') <= value.length - 3 || 'Email should have a valid domain extension'
+    ],
+    agreeToTermsRules: [
+      value => !!value || 'You must agree to all terms to continue to sign up an account!'
+    ],
+    agreeToTerms: false,
+    browsers: ["Chrome", "Firefox", "Safari", "Brave"], 
+    birthday: "",
+    formValidity: false
+  }),
+  methods: {
+    resetValidation() {
+      this.$refs.signUpFrom.resetValidation()
+    },
+    resetForm() {
+      this.$refs.signUpFrom.reset()
+    },
+    validateForm() {
+      this.$refs.signUpFrom.validate()
+    }
+  }
 };
 </script>
