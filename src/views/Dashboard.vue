60c46b7f3c356e7657643b9c3cbbf83331f6f950
luml <1520622848@qq.com>
2021-09-14 16:29:42 +0800

    feat: directives

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,9 +3,25 @@
     <h1>Dashboard</h1>
     <v-row>
       <v-col cols="12" v-for="item in sales" :key="item.label" md="3">
-        <v-card elevation="9" tile class="shopping-item" color="primary">
+        <v-card
+          v-ripple
+          elevation="9"
+          tile
+          class="shopping-item"
+          color="primary"
+        >
           <v-icon>{{ item.icon }}</v-icon>
-          <v-card-title>{{ item.label }}</v-card-title>
+          <v-img src="https://picsum.photos/510/300?random" aspect-ratio="1.7" contain>
+            <v-card-title>{{ item.label }}</v-card-title>
+            <template v-slot:placeholder>
+              <v-row class="fill-height ma-0" align="center" justify="center">
+                <v-progress-circular
+                  indeterminate
+                  color="grey lighten-5"
+                ></v-progress-circular>
+              </v-row>
+            </template>
+          </v-img>
         </v-card>
       </v-col>
     </v-row>
@@ -16,6 +32,26 @@
       class="elevation-1"
       @click:row="selectRow"
     ></v-data-table>
+    <v-row v-if="loadNewContent" v-intersect="showMoreContent">
+      <v-col cols="6" sm="4">
+        <v-img
+          src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg"
+          gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)"
+        ></v-img>
+      </v-col>
+
+      <v-col cols="6" sm="4">
+        <v-img src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg">
+          <div class="fill-height bottom-gradient"></div>
+        </v-img>
+      </v-col>
+
+      <v-col cols="6" sm="4">
+        <v-img src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg">
+          <div class="fill-height repeating-gradient"></div>
+        </v-img>
+      </v-col>
+    </v-row>
     <v-snackbar v-model="snackbar" :left="$vuetify.breakpoint.lgAndUp">
       You have selectd {{ currentItem }}
       <template v-slot:action="{ attrs }">
@@ -29,23 +65,24 @@
 export default {
   data() {
     return {
+      loadNewContent: true,
       sales: [
         {
-          label: 'Bags',
-          icon: 'mdi-bag-personal-outline',
+          label: "Bags",
+          icon: "mdi-bag-personal-outline",
         },
         {
-          label: 'Shoes',
-          icon: 'mdi-shoe-heel'
+          label: "Shoes",
+          icon: "mdi-shoe-heel",
         },
         {
-          label: 'Hats',
-          icon: 'mdi-account-cowboy-hat'
+          label: "Hats",
+          icon: "mdi-account-cowboy-hat",
         },
         {
-          label: 'Gloves',
-          icon: 'mdi-boxing-glove'
-        }
+          label: "Gloves",
+          icon: "mdi-boxing-glove",
+        },
       ],
       currentItem: "",
       snackbar: false,
@@ -55,13 +92,13 @@ export default {
           text: "Dessert (100g serving)",
           align: "start",
           sortable: false,
-          value: "name"
+          value: "name",
         },
         { text: "Calories", value: "calories" },
         { text: "Fat (g)", value: "fat" },
         { text: "Carbs (g)", value: "carbs" },
         { text: "Protein (g)", value: "protein" },
-        { text: "Iron (%)", value: "iron" }
+        { text: "Iron (%)", value: "iron" },
       ],
       desserts: [
         {
@@ -70,7 +107,7 @@ export default {
           fat: 6.0,
           carbs: 24,
           protein: 4.0,
-          iron: "1%"
+          iron: "1%",
         },
         {
           name: "Ice cream sandwich",
@@ -78,7 +115,7 @@ export default {
           fat: 9.0,
           carbs: 37,
           protein: 4.3,
-          iron: "1%"
+          iron: "1%",
         },
         {
           name: "Eclair",
@@ -86,7 +123,7 @@ export default {
           fat: 16.0,
           carbs: 23,
           protein: 6.0,
-          iron: "7%"
+          iron: "7%",
         },
         {
           name: "Cupcake",
@@ -94,7 +131,7 @@ export default {
           fat: 3.7,
           carbs: 67,
           protein: 4.3,
-          iron: "8%"
+          iron: "8%",
         },
         {
           name: "Gingerbread",
@@ -102,7 +139,7 @@ export default {
           fat: 16.0,
           carbs: 49,
           protein: 3.9,
-          iron: "16%"
+          iron: "16%",
         },
         {
           name: "Jelly bean",
@@ -110,7 +147,7 @@ export default {
           fat: 0.0,
           carbs: 94,
           protein: 0.0,
-          iron: "0%"
+          iron: "0%",
         },
         {
           name: "Lollipop",
@@ -118,7 +155,7 @@ export default {
           fat: 0.2,
           carbs: 98,
           protein: 0,
-          iron: "2%"
+          iron: "2%",
         },
         {
           name: "Honeycomb",
@@ -126,7 +163,7 @@ export default {
           fat: 3.2,
           carbs: 87,
           protein: 6.5,
-          iron: "45%"
+          iron: "45%",
         },
         {
           name: "Donut",
@@ -134,7 +171,7 @@ export default {
           fat: 25.0,
           carbs: 51,
           protein: 4.9,
-          iron: "22%"
+          iron: "22%",
         },
         {
           name: "KitKat",
@@ -142,23 +179,42 @@ export default {
           fat: 26.0,
           carbs: 65,
           protein: 7,
-          iron: "6%"
-        }
-      ]
+          iron: "6%",
+        },
+      ],
     };
   },
   methods: {
     selectRow(event) {
       this.snackbar = true;
       this.currentItem = event.name;
-    }
-  }
+    },
+    showMoreContent(entries) {
+      // More information about these options
+      // is located here: https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+      // this.loadNewContent = entries[0].isIntersecting;
+      console.log(entries)
+    },
+  },
 };
 </script>
 
 <style scoped>
 .shopping-item {
-  height: 10rem;
+  height: auto;
   margin: 2px;
+}
+.bottom-gradient {
+  background-image: linear-gradient(to top, rgba(0, 0, 0, 0.4) 0%, transparent 72px);
+}
+
+.repeating-gradient {
+  background-image: repeating-linear-gradient(
+    -45deg,
+    rgba(255, 0, 0, 0.25),
+    rgba(255, 0, 0, 0.25) 5px,
+    rgba(0, 0, 255, 0.25) 5px,
+    rgba(0, 0, 255, 0.25) 10px
+  );
 }
 </style>
